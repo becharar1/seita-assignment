c3b77a15ece568fdb7a9b255a7bfd235e7ecb780
becharar1 <60255285+becharar1@users.noreply.github.com>
2023-01-09 19:25:26 +0100

    Update seita1.py

--- seita1.py
+++ seita1.py
@@ -8,35 +8,25 @@ from pandas.io.formats.style_render import DataFrame
 from datetime import datetime
 from datetime import timedelta
 df=pd.read_csv("weather.csv", delimiter= ',')
+sensors=['temperatre','irradiance','wind speed']
 '''
 This is the function that enables to get the relevant forecasts
 '''
-def get_forecasts(now,then,df=df): 
+def get_forecasts(now,then,df=df,sensors=sensors): 
   ''' 
   The dates are first parsed from string to datetime
-  The difference in time is calculated
+  The difference in time between then and now is calculated
   The data relevant to the specified then date is extracted
-  The data is then sorted in ascending order according to timely_beliefs
+  The data is then sorted in ascending order according to timely_beliefs and filtered to remove any forecasts newer than now
+  The first measurements relevant to each sensor were extracted from the obtained dataframe
   '''
   time_now=datetime.strptime(now,'%Y-%m-%d%H:%M:%S')
   time_then=datetime.strptime(then,'%Y-%m-%d%H:%M:%S')
   diff=(time_then-time_now).total_seconds()
   dfthen = df.loc[df['event_start'].str.contains(str(time_then), case=False)]
-  dfthen=dfthen.sort_values(by="belief_horizon_in_sec")
-  '''
-  a loop is here realized to obtain at max the first three forecasts with timely_beliefs greater than the difference
-  This corresponds to three most relevant values. The program indicates also if no values are found 
-  '''
-  i=0
-  ipertinent=0
-  dfpertinent = pd.DataFrame(columns=['event_value','sensor','unit'])
-  while i<len(dfthen) and ipertinent<3:
-    if dfthen.iloc[i,1]>diff:
-      dfpertinent.loc[ipertinent]=dfthen.iloc[i,2:5]
-      ipertinent+=1
-    i=i+1
+  dfthen_sorted_filtered=dfthen.sort_values(by="belief_horizon_in_sec").query('belief_horizon_in_sec>'+str(diff))
+  dfpertinent=dfthen_sorted_filtered.groupby("sensor").first()
   return (dfpertinent.to_json(orient = 'records',force_ascii=False,lines=True))
-
 '''
 Here the route is specified using flask with two inputs: now and then
 '''
